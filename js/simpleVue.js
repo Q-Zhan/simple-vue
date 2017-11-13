6b046ed2be36c67ec9ff0cf94907d721dc0efabe
Q-Zhan <380776767@qq.com>
2017-11-13 20:30:56 +0800

    SimpleVue添加属性代理

--- js/simpleVue.js
+++ js/simpleVue.js
@@ -1,9 +1,29 @@
 function SimpleVue (data, el, exp) {
+  var self = this
   this.data = data
+  Object.keys(data).forEach(function(key) {
+    self.proxyKeys(key)
+  })
   observe(data)
   el.innerHTML = this.data[exp]
   new Watcher(this, exp, function(value) {
     el.innerHTML = value
   })
   return this
+}
+
+SimpleVue.prototype = {
+  proxyKeys: function(key) {
+    var self = this
+    Object.defineProperty(this, key, {
+      enumerable: false,
+      configurable: true,
+      get: function() {
+        return self.data[key]
+      },
+      set: function(newVal) {
+        self.data[key] = newVal
+      }
+    })
+  }
 }
